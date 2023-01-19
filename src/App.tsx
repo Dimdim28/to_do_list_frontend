7ce3c287118dce062fc26e9f8da1f33f0cb4c431
Dimdim28 <wwwdimanes1@gmail.com>
2023-01-19 16:55:31 +0200

    Added icon and header logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React, { Suspense  } from "react";
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 import Header from "./components/Header/Header";
 import Category from "./pages/Category/Category";
 import Home from "./pages/Home/Home";
@@ -13,17 +13,17 @@ import Task from "./pages/Task/Task";
 function App() {
   return (
     <div className="App">
-     <Header/>
-     <Suspense fallback={<div>loading</div> }>
-                <Routes>
-                  <Route path="/profile" element={<Profile />} />
-                  <Route path="/category" element={<Category />} />
-                  <Route path="/task" element={<Task />} />
-                  <Route path="/register" element={<Register />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/" element={<Home />} />
-                </Routes>
-              </Suspense>
+      <Header />
+      <Suspense fallback={<div>loading</div>}>
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/task" element={<Task />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.scss";
+import logo from "../../assets/logo.png";
 const Header: React.FC = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <header>
+      <img className={styles.logo} alt="logo" src={logo} />
+      <nav>
+        <NavLink
+          to={"profile"}
+          className={({ isActive }) => (isActive ? styles.isActive : "")}
+        >
+          Profile
+        </NavLink>
+        <NavLink
+          to={"/"}
+          className={({ isActive }) => (isActive ? styles.isActive : "")}
+        >
+          Home
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
