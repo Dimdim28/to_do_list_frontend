a23d33306bf519dc6a8df220330807818f556e2e
Dimdim28 <wwwdimanes1@gmail.com>
2023-01-27 00:02:28 +0200

    Added redirecting beetween pages

--- src/pages/Task/Task.tsx
+++ src/pages/Task/Task.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Navigate } from "react-router";
+import { useAppSelector } from "../../hooks";
+import { selectIsAuth } from "../../redux/slices/auth/selectors";
 
-const Task:React.FC = () => {
-  return (
-    <div>
-      Task
-    </div>
-  )
-}
+const Task: React.FC = () => {
+  const isAuth = useAppSelector(selectIsAuth);
+  if (!isAuth) return <Navigate to="/auth/login" />;
 
-export default Task
+  return <div>Task</div>;
+};
+
+export default Task;
