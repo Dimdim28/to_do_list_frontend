e38b8837145d57dc3680bd6ebc052de083049541
den <denissytnikmacho@gmail.com>
2023-02-17 16:25:14 +0200

    added task modal window mark-up

---
+++ src/pages/Home/Tasks/TaskDeleting/TaskDeleting.tsx
@@ -0,0 +1,28 @@
+import styles from './TaskDeleting.module.scss';
+import Button from '../../../../components/common/Button/Button';
+import Task from '../../../Task/Task';
+
+interface TaskDeletingProps {
+  toggleActive: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const TaskDeleting: React.FC<TaskDeletingProps> = ({ toggleActive }) => {
+  const submitHandler = () => {
+
+  };
+
+  const cancelHandler = () => {
+    toggleActive(false)
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.actions}>
+        <Button text='cancel' callback={() => {}} class='cancel' />
+        <Button text='submit' callback={cancelHandler} class='submit' />
+      </div>
+    </div>
+  )
+};
+
+export default TaskDeleting;
--- src/pages/Home/Tasks/Tasks.tsx
+++ src/pages/Home/Tasks/Tasks.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Tasks.module.scss";
+import TaskDeleting from "./TaskDeleting/TaskDeleting";
+import { Modal } from "../../../components/common/Modal/Modal";
 
 const Tasks = () => {
-  return <main className={styles.wrapper}>tasks</main>;
+  // const [taskEditing, setTaskEditing] = useState(false);
+  const [taskDeleting, setTaskDeleting] = useState(true);
+  const [taskProps, setTaskProps] = useState({});
+
+  return (
+    <main className={styles.wrapper}>
+      <Modal
+        active={taskDeleting}
+        setActive={setTaskDeleting}
+        ChildComponent={TaskDeleting}
+        childProps={taskProps}
+      />
+      tasks
+    </main>
+  );
 };
 
 export default Tasks;
