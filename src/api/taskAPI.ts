2557d4581c4ea833691b15ba1d6ce328effd86dc
Dimdim28 <wwwdimanes1@gmail.com>
2023-04-17 17:53:16 +0300

    feature: added API for creating task

--- src/api/taskAPI.ts
+++ src/api/taskAPI.ts
@@ -33,7 +33,68 @@ interface AddTask extends PureTask, User {}
 export interface Task extends PureTask, Id, User, Date {}
 
 export interface Result {
-  Task: Task | null;
+  task: Task | null;
   status: Status;
   message?: string;
 }
+
+class taskAPIClass {
+  public async deletetask(id: string): Promise<Result> {
+    try {
+      const response: TaskResponse = await instanse.delete(`/task/${id}`);
+      return { task: response.data, status: Status.SUCCESS };
+    } catch (err: any) {
+      return {
+        message: err.response.data.message,
+        status: Status.ERROR,
+        task: null,
+      };
+    }
+  }
+
+  public async addtask(params: AddTask): Promise<Result> {
+    const { title, description, categories, user, deadline, isCompleted } =
+      params;
+    try {
+      console.log(params);
+      const response: TaskResponse = await instanse.post("/task", {
+        title,
+        user,
+        description,
+        categories,
+        deadline,
+        isCompleted,
+      });
+      return { task: response.data, status: Status.SUCCESS };
+    } catch (err: any) {
+      return {
+        message: err.response.data.message,
+        status: Status.ERROR,
+        task: null,
+      };
+    }
+  }
+
+  public async edittask(params: EditTask): Promise<Result> {
+    const { title, description, categories, _id, deadline, isCompleted } =
+      params;
+    try {
+      const response: TaskResponse = await instanse.patch(`/task/${_id}`, {
+        title,
+        description,
+        categories,
+        deadline,
+        isCompleted,
+      });
+      return { task: response.data, status: Status.SUCCESS };
+    } catch (err: any) {
+      return {
+        message: err.response.data.message,
+        status: Status.ERROR,
+        task: null,
+      };
+    }
+  }
+}
+const taskAPI = new taskAPIClass();
+export default taskAPI;
--- src/pages/Home/Tasks/TaskEditing/TaskForm.tsx
+++ src/pages/Home/Tasks/TaskEditing/TaskForm.tsx
@@ -1,12 +1,13 @@
 import styles from "./TaskForm.module.scss";
 import Button from "../../../../components/common/Button/Button";
-import { Task } from "../../../../api/taskAPI";
+import taskAPI, { Task } from "../../../../api/taskAPI";
 import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../../hooks";
 import { selectProfile } from "../../../../redux/slices/auth/selectors";
 import { Status } from "../../../../types";
 import { Input } from "../../../../components/common/Input/Input";
 import { Checkbox } from "../../../../components/common/Checkbox/Checkbox";
+import Preloader from "../../../../components/Preloader/Preloader";
 
 interface TaskFormProps {
   toggleActive: React.Dispatch<React.SetStateAction<boolean>>;
@@ -34,7 +35,31 @@ const TaskForm: React.FC<TaskFormProps> = ({ toggleActive, childProps }) => {
   const [hasDeadline, setHasDeadline] = useState(!!prevDeadline);
   const [deadline, setDeadline] = useState(prevDeadline || "");
   const [isCompleted, setIsCompleted] = useState(prevIscompleted || false);
-  const submit = () => {};
+
+  const submit = async () => {
+    setStatus(Status.LOADING);
+
+    let payload = { title, description };
+    if (hasDeadline && deadline) payload = Object.assign(payload, { deadline });
+    if (categories.length > 0) payload = Object.assign(payload, { categories });
+    if (isCompleted) payload = Object.assign(payload, { isCompleted });
+
+    console.log(payload);
+    const result = _id
+      ? await taskAPI.edittask({ _id, ...payload })
+      : await taskAPI.addtask({ user: userId, ...payload });
+    const { message, status } = result;
+    setStatus(status);
+    setTaskError(message || "");
+    if (status === Status.SUCCESS) {
+      toggleActive(false);
+      // if (_id) {
+      //   dispatch(updateCategoryInList({ _id, title, color }));
+      // } else {
+      //   dispatch(addCategoryToList(result.category));
+      // }
+    }
+  };
 
   const cancel = () => {
     toggleActive(false);
@@ -42,37 +67,44 @@ const TaskForm: React.FC<TaskFormProps> = ({ toggleActive, childProps }) => {
 
   return (
     <div className={styles.wrapper}>
-      <Input title="title" value={title} setValue={setTittle} type="text" />
-      <Input
-        title="description"
-        value={description}
-        setValue={setDescription}
-        type="text"
-      />
-      <Checkbox
-        isChecked={hasDeadline}
-        setIsChecked={setHasDeadline}
-        label="Task has deadline"
-      />
-      {hasDeadline && (
-        <input
-          type="date"
-          className={styles.inputDate}
-          value={deadline}
-          onChange={(e) => setDeadline(e.target.value)}
-        />
+      {status === Status.LOADING ? (
+        <Preloader />
+      ) : (
+        <>
+          <Input title="title" value={title} setValue={setTittle} type="text" />
+          <Input
+            title="description"
+            value={description}
+            setValue={setDescription}
+            type="text"
+          />
+          <Checkbox
+            isChecked={hasDeadline}
+            setIsChecked={setHasDeadline}
+            label="Task has deadline"
+          />
+          {hasDeadline && (
+            <input
+              type="date"
+              className={styles.inputDate}
+              value={deadline}
+              onChange={(e) => setDeadline(e.target.value)}
+            />
+          )}
+
+          <Checkbox
+            isChecked={isCompleted}
+            setIsChecked={setIsCompleted}
+            label="Task completed"
+          />
+
+          <div className={styles.actions}>
+            <Button text="cancel" callback={cancel} class="cancel" />
+            <Button text="submit" callback={submit} class="submit" />
+          </div>
+          {taskError && <p className={styles.error}>{taskError}</p>}
+        </>
       )}
-
-      <Checkbox
-        isChecked={isCompleted}
-        setIsChecked={setIsCompleted}
-        label="Task completed"
-      />
-
-      <div className={styles.actions}>
-        <Button text="cancel" callback={cancel} class="cancel" />
-        <Button text="submit" callback={submit} class="submit" />
-      </div>
     </div>
   );
 };
