60271b5f1db5a242cbbbd8b3447de94f48cde518
den <denissytnikmacho@gmail.com>
2023-05-21 15:18:05 +0300

    fix: fixed date

--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -1,7 +1,7 @@
 export const humaniseDate = (date: string) => {
     const DateInstance = new Date(date);
     let day: string | number = DateInstance.getDate();
-    let month: string | number = DateInstance.getMonth();
+    let month: string | number = DateInstance.getMonth() + 1;
     let year = DateInstance.getFullYear();
     if (day < 10) day = '0' + day;
     if (month < 10) month = '0' + month;
