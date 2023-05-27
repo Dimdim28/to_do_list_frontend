e515a2049d9ed059d8826ef6701c1ecde69da198
LikerFeed <vanya18845@gmail.com>
2023-05-27 16:18:24 +0300

    added selectCategoryTotalPages homeSlice selector

--- src/redux/slices/home/selectors.test.js
+++ src/redux/slices/home/selectors.test.js
@@ -20,5 +20,11 @@ import {
       expect(selectCategoryCurrentPage(MOCK_OBJECT_TWO)).toBe(1);
       expect(selectCategoryCurrentPage(MOCK_OBJECT_THREE)).toBe(1);
     });
+
+    it("selectCategoryTotalPages must work correctly", () => {
+      expect(selectCategoryTotalPages(MOCK_OBJECT_ONE)).toBe(5);
+      expect(selectCategoryTotalPages(MOCK_OBJECT_TWO)).toBe(0);
+      expect(selectCategoryTotalPages(MOCK_OBJECT_THREE)).toBe(0);
+    });
   });
   
