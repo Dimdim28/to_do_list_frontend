97a806d906f259dbce82ad88781f7c2c8b07a3a6
den <denissytnikmacho@gmail.com>
2023-05-27 16:49:32 +0300

    test: added clearProfileErrorMessage profileSlice reducer

--- src/redux/slices/profile/reducers.test.ts
+++ src/redux/slices/profile/reducers.test.ts
@@ -8,13 +8,9 @@ import { profileReducer, exit, clearProfileErrorMessage } from "./profile";
 
 describe("Testing profile slice reducers", () => {
   const exitAction = { type: exit.type };
-  const clearProfileAction = { type: clearProfileErrorMessage.type };
+  const clearProfileErrorMessageAction = { type: clearProfileErrorMessage.type };
 
   describe("exit reducer must works currectly", () => {
-    beforeEach(() => {
-      localStorage.setItem("token", "myToken");
-    });
-
     it("Profile must be cleared after this reducer calling", () => {
       const firstResult = profileReducer(MOCK_OBJECT_ONE.profile, exitAction);
       const secondResult = profileReducer(MOCK_OBJECT_TWO.profile, exitAction);
@@ -23,6 +19,18 @@ describe("Testing profile slice reducers", () => {
       expect(firstResult.data).toBe(null);
       expect(secondResult.data).toBe(null);
       expect(thirdResult.data).toBe(null);
+    });
+  });
+
+  describe("clearProfileErrorMessage reducer must works currectly", () => {
+    it("Profile error message must be cleared after this reducer calling", () => {
+      const firstResult = profileReducer(MOCK_OBJECT_ONE.profile, clearProfileErrorMessageAction);
+      const secondResult = profileReducer(MOCK_OBJECT_TWO.profile, clearProfileErrorMessageAction);
+      const thirdResult = profileReducer(MOCK_OBJECT_THREE.profile, clearProfileErrorMessageAction);
+
+      expect(firstResult.message).toBe("");
+      expect(secondResult.message).toBe("");
+      expect(thirdResult.message).toBe("");
     });
   });
 });
