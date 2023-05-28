ab3ccb3b1f67c21b8110198e02b80dc540d251c2
Dimdim28 <wwwdimanes1@gmail.com>
2023-05-28 17:25:34 +0300

    Fix dependency

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -42,7 +42,7 @@ const Home: React.FC = () => {
     return () => {
       dispatch(clearCategories());
     };
-  }, [dispatch]);
+  }, []);
 
   async function fetchTasks(params?: getTask) {
     setIsTasksLoading(true);
