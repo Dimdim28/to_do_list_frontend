38ea097ecb1dddaedc16a5145449154141c0b54d
LikerFeed <vanya18845@gmail.com>
2023-05-31 13:26:10 +0300

    test: improved clearCategories homeSlice reducer

--- src/redux/slices/home/reducers.test.ts
+++ src/redux/slices/home/reducers.test.ts
@@ -91,34 +91,41 @@ describe("Testing home slice reducers", () => {
       clearCategoriesAction
     );
 
+    const fourthResult = homeReducer(initialState, clearCategoriesAction);
+
     it("categories list must be empty", () => {
       expect(firstResult.category.categories).toEqual([]);
       expect(secondResult.category.categories).toEqual([]);
       expect(thirdResult.category.categories).toEqual([]);
+      expect(fourthResult.category.categories).toEqual([]);
     });
 
     it("totalPages must stay the same", () => {
       expect(firstResult.category.totalPages).toBe(5);
       expect(secondResult.category.totalPages).toBe(0);
       expect(thirdResult.category.totalPages).toBe(0);
+      expect(fourthResult.category.totalPages).toBe(4);
     });
 
     it("currentPage must stay the same", () => {
       expect(firstResult.category.currentPage).toBe(4);
       expect(secondResult.category.currentPage).toBe(1);
       expect(thirdResult.category.currentPage).toBe(1);
+      expect(fourthResult.category.currentPage).toBe(4);
     });
 
     it("status must stay the same", () => {
       expect(firstResult.category.status).toBe(Status.SUCCESS);
       expect(secondResult.category.status).toBe(Status.LOADING);
       expect(thirdResult.category.status).toBe(Status.ERROR);
+      expect(fourthResult.category.status).toBe(Status.SUCCESS);
     });
 
     it("message must stay the same", () => {
       expect(firstResult.category.message).toBe("");
       expect(secondResult.category.message).toBe("");
       expect(thirdResult.category.message).toBe("Error");
+      expect(fourthResult.category.message).toBe("");
     });
   });
 
