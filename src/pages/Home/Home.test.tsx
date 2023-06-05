8d542f3a52c8e367492af440111e8cabf6633ec6
Dimdim28 <wwwdimanes1@gmail.com>
2023-06-06 00:26:55 +0300

    test: added Home

---
+++ src/pages/Home/Home.test.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import Home from "./Home";
+import store from "../../redux/store";
+
+describe("Home", () => {
+  test("renders without errors", () => {
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
+
+    expect(screen.getByText("Categories")).toBeInTheDocument();
+    expect(screen.getByText("Create Category +")).toBeInTheDocument();
+    expect(screen.getByText("Date and status")).toBeInTheDocument();
+    expect(screen.getByText("Deadline filters")).toBeInTheDocument();
+    expect(screen.getByText("Completion status")).toBeInTheDocument();
+  });
+});
