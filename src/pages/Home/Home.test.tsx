7196763f0ab1417a0bb409182468dda53c296292
Valentyn13 <valentyn13git@gmail.com>
2023-07-26 17:09:57 +0300

    refactor: all imports in pages/Home

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
+import { render, screen } from "@testing-library/react";
+
 import Home from "./Home";
 import store from "../../redux/store";
 
