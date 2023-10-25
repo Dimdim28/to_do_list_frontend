cd9eb331077a4f7edae698082676c2b9fa829cde
den <denissytnikmacho@gmail.com>
2023-10-25 14:55:14 +0300

    fix: changed mock data avatar field

--- src/mocs/state.ts
+++ src/mocs/state.ts
@@ -1,48 +1,48 @@
-import { RootState } from "../redux/store";
-import { Language, Status, Theme } from "../types";
+import { RootState } from '../redux/store';
+import { Language, Status, Theme } from '../types';
 
 export const MOCK_OBJECT_ONE: RootState = {
   auth: {
     status: Status.SUCCESS,
     profile: {
-      _id: "123456",
-      createdAt: "2007 year",
-      updatedAt: "2008 year",
-      email: "test@gmail.com",
-      token: "01032004hahathisismytoken",
-      username: "DimonTheBest",
+      _id: '123456',
+      createdAt: '2007 year',
+      updatedAt: '2008 year',
+      email: 'test@gmail.com',
+      token: '01032004hahathisismytoken',
+      username: 'DimonTheBest',
     },
     theme: Theme.DARK,
     lang: Language.EN,
-    message: "",
+    message: '',
   },
   home: {
     category: {
       categories: [
         {
-          _id: "646b95736b2cb6353f4fd104",
-          title: "hello",
-          user: "63f6342acc86923016194255",
-          color: "#d82222",
+          _id: '646b95736b2cb6353f4fd104',
+          title: 'hello',
+          user: '63f6342acc86923016194255',
+          color: '#d82222',
         },
         {
-          _id: "646bbbaefedb212d312d0447",
-          title: "lalala",
-          user: "63f6342acc86923016194255",
-          color: "#16a29f",
+          _id: '646bbbaefedb212d312d0447',
+          title: 'lalala',
+          user: '63f6342acc86923016194255',
+          color: '#16a29f',
         },
       ],
       totalPages: 5,
       currentPage: 4,
       status: Status.SUCCESS,
-      message: "",
+      message: '',
     },
   },
   profile: {
     stats: [],
     data: null,
     status: Status.LOADING,
-    message: "",
+    message: '',
   },
 };
 
@@ -50,7 +50,7 @@ export const MOCK_OBJECT_TWO: RootState = {
   auth: {
     status: Status.ERROR,
     profile: null,
-    message: "this is an error",
+    message: 'this is an error',
     theme: Theme.LIGHT,
     lang: Language.UA,
   },
@@ -60,25 +60,28 @@ export const MOCK_OBJECT_TWO: RootState = {
       totalPages: 0,
       currentPage: 1,
       status: Status.LOADING,
-      message: "",
+      message: '',
     },
   },
   profile: {
     stats: [
-      { counter: 6, date: "16" },
-      { counter: 4, date: "2" },
-      { counter: 8, date: "100" },
+      { counter: 6, date: '16' },
+      { counter: 4, date: '2' },
+      { counter: 8, date: '100' },
     ],
     data: {
-      _id: "6460e2e3832ea98269aa3777",
-      username: "dench",
-      email: "dench@gmail.com",
-      createdAt: "2023",
-      updatedAt: "2024",
-      avatarUrl: "link",
+      _id: '6460e2e3832ea98269aa3777',
+      username: 'dench',
+      email: 'dench@gmail.com',
+      createdAt: '2023',
+      updatedAt: '2024',
+      avatar: {
+        url: 'link',
+        public_id: 'id',
+      },
     },
     status: Status.SUCCESS,
-    message: "",
+    message: '',
   },
 };
 
@@ -95,13 +98,13 @@ export const MOCK_OBJECT_THREE: RootState = {
       totalPages: 0,
       currentPage: 1,
       status: Status.ERROR,
-      message: "Error",
+      message: 'Error',
     },
   },
   profile: {
     stats: [],
     data: null,
     status: Status.ERROR,
-    message: "Error",
+    message: 'Error',
   },
 };
