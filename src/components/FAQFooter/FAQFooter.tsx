569d29daeee50b8d5519fbf8c8b33f2197c650a5
Dimdim28 <wwwdimanes1@gmail.com>
2023-11-03 14:34:18 +0200

    refactor: added links list rendering in FAQFooter

--- src/components/FAQFooter/FAQFooter.tsx
+++ src/components/FAQFooter/FAQFooter.tsx
@@ -6,24 +6,34 @@ import ROUTES from "../../routes";
 
 import styles from "./FAQFooter.module.scss";
 
+const LINKS = [
+  {
+    path: ROUTES.FAQ,
+    name: "faq",
+  },
+  {
+    path: ROUTES.HOME,
+    name: "back",
+  },
+];
+
 const FAQFooter: FC = () => {
   const { t } = useTranslation();
   return (
     <footer className={styles.footer}>
-      <div className={styles.leftAlignedText}>
-        <NavLink
-          to={ROUTES.FAQ}
-          className={({ isActive }) => (isActive ? styles.isActive : "")}
-        >
-          {t("faq")}
-        </NavLink>
-        <NavLink
-          to={ROUTES.HOME}
-          className={({ isActive }) => (isActive ? styles.isActive : "")}
-        >
-          {t("back")}
-        </NavLink>
-      </div>
+      <nav className={styles.leftAlignedText}>
+        {LINKS.map(({ path, name }) => (
+          <NavLink
+            key={name}
+            to={path}
+            className={({ isActive }) =>
+              isActive ? styles.isActive : undefined
+            }
+          >
+            {t(name)}
+          </NavLink>
+        ))}
+      </nav>
       <div className={styles.centeredText}>{t("footer")}</div>
     </footer>
   );
