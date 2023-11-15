879fb518dbe07e033a067f51892c6704e2faf511
LikerFeed <vanya18845@gmail.com>
2023-11-15 19:26:04 +0200

    test: refactored Avatar tests

--- src/pages/Profile/ProfileCard/Avatar/Avatar.test.tsx
+++ src/pages/Profile/ProfileCard/Avatar/Avatar.test.tsx
@@ -5,52 +5,34 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Avatar from "./Avatar";
 import store from "../../../../redux/store";
 
-describe("Avatar", () => {
-  test("renders without crashing", () => {
-    render(
-      <Provider store={store}>
-        <Router>
-          <Avatar />
-        </Router>
-      </Provider>
-    );
+const renderAvatar = () => {
+  render(
+    <Provider store={store}>
+      <Router>
+        <Avatar />
+      </Router>
+    </Provider>
+  );
+};
 
+describe("Avatar", () => {
+  beforeEach(() => {
+    renderAvatar();
+  });
+
+  test("renders without crashing", () => {
     expect(screen.getByTestId("avatar-component")).toBeInTheDocument();
   });
 
   test("displays the file input", () => {
-    render(
-      <Provider store={store}>
-        <Router>
-          <Avatar />
-        </Router>
-      </Provider>
-    );
-
     expect(screen.getByTestId("file-input")).toBeInTheDocument();
   });
 
   test("displays the add photo button", () => {
-    render(
-      <Provider store={store}>
-        <Router>
-          <Avatar />
-        </Router>
-      </Provider>
-    );
-
     expect(screen.getByTestId("add-photo")).toBeInTheDocument();
   });
 
   test("displays the camera icon", () => {
-    render(
-      <Provider store={store}>
-        <Router>
-          <Avatar />
-        </Router>
-      </Provider>
-    );
-
     expect(screen.getByTestId("camera-icon")).toBeInTheDocument();
   });
 });
