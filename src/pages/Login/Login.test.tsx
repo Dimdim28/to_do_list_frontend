f96d5ad5afd93b1781c967dc2ee7afceab65c2d8
Dimdim28 <wwwdimanes1@gmail.com>
2025-04-06 21:46:06 +0300

    test: fixed tests

--- src/pages/Login/Login.test.tsx
+++ src/pages/Login/Login.test.tsx
@@ -1,24 +1,29 @@
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
-import { render, screen } from "@testing-library/react";
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { render, screen } from '@testing-library/react';
 
-import store from "../../redux/store";
+import store from '../../redux/store';
 
-import Login from "./Login";
+import Login from './Login';
 
-describe("Login", () => {
-  test("renders the login form", () => {
+const clientID = process.env.REACT_GOOGLE_CLIENT_ID || '';
+
+describe('Login', () => {
+  test('renders the login form', () => {
     render(
-      <Provider store={store}>
-        <Router>
-          <Login />
-        </Router>
-      </Provider>
+      <GoogleOAuthProvider clientId={clientID}>
+        <Provider store={store}>
+          <Router>
+            <Login />
+          </Router>
+        </Provider>{' '}
+      </GoogleOAuthProvider>,
     );
 
-    expect(screen.getByText("email")).toBeInTheDocument();
-    expect(screen.getByText("password")).toBeInTheDocument();
-    expect(screen.getByText("signIn")).toBeInTheDocument();
-    expect(screen.getByText("signUp")).toBeInTheDocument();
+    expect(screen.getByText('email')).toBeInTheDocument();
+    expect(screen.getByText('password')).toBeInTheDocument();
+    expect(screen.getByText('signIn')).toBeInTheDocument();
+    expect(screen.getByText('signUp')).toBeInTheDocument();
   });
 });
